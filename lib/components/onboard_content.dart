bfab08b1cac9f22b90a99ca8679a4617fce74e3f
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-03-14 07:52:05 +0600

    gesture dectector added on btn

--- lib/components/onboard_content.dart
+++ lib/components/onboard_content.dart
@@ -49,55 +49,64 @@ class _OnboardContentState extends State<OnboardContent> {
             height: 56,
             bottom: 32 + progress * 140,
             right: 16,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-                gradient: LinearGradient(
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                  stops: [0.4, 0.8],
-                  colors: [
-                    Color.fromARGB(255, 239, 104, 80),
-                    Color.fromARGB(255, 139, 33, 146)
-                  ],
+            child: GestureDetector(
+              onTap: () {
+                if (_pageController.page == 0) {
+                  _pageController.animateToPage(1,
+                      duration: const Duration(milliseconds: 400),
+                      curve: Curves.ease);
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                    stops: [0.4, 0.8],
+                    colors: [
+                      Color.fromARGB(255, 239, 104, 80),
+                      Color.fromARGB(255, 139, 33, 146)
+                    ],
+                  ),
                 ),
-              ),
-              child: DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 92 + progress * 32,
-                      child: Stack(
-                        fit: StackFit.passthrough,
-                        children: [
-                          Opacity(
-                            opacity: 1 - progress,
-                            child: Text("Get Started"),
-                          ),
-                          Opacity(
-                            opacity: progress,
-                            child: Text(
-                              "Create account",
-                              maxLines: 1,
-                              overflow: TextOverflow.fade,
-                              softWrap: false,
+                child: DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 92 + progress * 32,
+                        child: Stack(
+                          fit: StackFit.passthrough,
+                          children: [
+                            Opacity(
+                              opacity: 1 - progress,
+                              child: const Text("Get Started"),
                             ),
-                          ),
-                        ],
+                            Opacity(
+                              opacity: progress,
+                              child: const Text(
+                                "Create account",
+                                maxLines: 1,
+                                overflow: TextOverflow.fade,
+                                softWrap: false,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    const Icon(
-                      Icons.chevron_right,
-                      size: 24,
-                      color: Colors.white,
-                    )
-                  ],
+                      const Icon(
+                        Icons.chevron_right,
+                        size: 24,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
