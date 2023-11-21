24f5a3176f8ae064c717e4457efc6f9db5982b9e
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2023-11-21 09:11:25 +0600

    const added and _TextField remove

--- lib/components/sing_up_form.dart
+++ lib/components/sing_up_form.dart
@@ -37,7 +37,7 @@ class SignUpForm extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(height: 24),
+                  const SizedBox(height: 24),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
@@ -65,54 +65,6 @@ class SignUpForm extends StatelessWidget {
             )
           ],
         ),
-      ),
-    );
-  }
-}
-
-class _TextField extends StatelessWidget {
-  final String label;
-  final IconData icon;
-  final bool hidden;
-
-  const _TextField({
-    Key? key,
-    required this.label,
-    required this.icon,
-    this.hidden = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
-      child: Row(
-        children: [
-          Flexible(
-            child: TextField(
-              cursorColor: Colors.pink.shade400,
-              obscureText: hidden,
-              autocorrect: !hidden,
-              enableSuggestions: !hidden,
-              decoration: InputDecoration(
-                labelText: label,
-                labelStyle: TextStyle(
-                  color: Colors.grey.shade400,
-                  fontSize: 18,
-                ),
-                suffixIcon: Icon(
-                  icon,
-                  color: Colors.grey.shade400,
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.pink.shade400,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
